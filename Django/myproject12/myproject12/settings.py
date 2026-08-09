5a89066bb9ebe40cd9e0ebd050ec871dd9bd20f1
Aditya94068 <adityavaishnav633@gmail.com>
2026-08-09 21:55:21 +0530

    adding css property in myproject12

--- Django/myproject12/myproject12/settings.py
+++ Django/myproject12/myproject12/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'portfolio'
+    'portfolio',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'myproject12.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
